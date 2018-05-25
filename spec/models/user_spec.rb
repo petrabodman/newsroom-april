2da2d3f896fa38dc4f952dbac2ff059c066e9a23
George_Lewis <georgejlewis@hotmail.com>
2018-05-25 11:06:02 +0200

    add user to factory bot and add rspec test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe 'Database table' do
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :encrypted_password }
+  end
 end
