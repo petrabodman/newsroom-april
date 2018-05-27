4b230552eaafdf08542e2a825501f32d08891b68
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-05-27 11:43:14 +0200

    Set up tailwind add weppack gem modify files for styling with tailwind

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2018_05_25_122609) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_articles_on_category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +47,5 @@ ActiveRecord::Schema.define(version: 2018_05_25_122609) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "articles", "categories"
 end
