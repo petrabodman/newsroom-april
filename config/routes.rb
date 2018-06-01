7a825198337533a38190258150af5704ae45fca8
George_Lewis <georgejlewis@hotmail.com>
2018-06-01 10:21:25 +0200

    amend syntax errors

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registration: "registrations" }
   resources :articles, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:new, :create, :index]
-  end
+end
   resources :categories, only: [:show]
   root controller: :articles, action: :index
-end
   resources :subscriptions, only: [:new, :create]
 end
