386f90680691a8ea0565cb95e760bf7a1ab322ac
magnus-thor <magnus.thor.ar@gmail.com>
2018-06-04 14:06:30 +0200

    commented out helper file for ES add a conditional to not run ES on host 9250 if CI

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,10 +1,22 @@
-Before('@search') do
-  Chewy.strategy(:bypass)
-  ArticleIndex.delete! if ArticleIndex.exists?
-  Elasticsearch::Extensions::Test::Cluster.start(
-    port: 9250,
-    nodes: 1,
-    timeout: 120
-  ) unless Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
-  ArticleIndex.create! unless ArticleIndex.exists?
-end
+# Before('@search') do
+#   Chewy.strategy(:bypass)
+#   ArticleIndex.delete! if ArticleIndex.exists?
+#   if use_test_cluster? 
+#     Elasticsearch::Model.client = Elasticsearch::Client.new(host: "localhost:9250")
+#     Elasticsearch::Extensions::Test::Cluster.start(
+#       port: 9250,
+#       nodes: 1,
+#       timeout: 120
+#     ) 
+#   end
+#   ArticleIndex.create! unless ArticleIndex.exists?
+# end
+
+# def use_test_cluster?
+#   !ENV["CI"] && !Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
+# end
+
+# if use_test_cluster? 
+#   Elasticsearch::Model.client = Elasticsearch::Client.new(host: "localhost:9250")
+#   Elasticsearch::Extensions::Test::Cluster.start(port: 9250, nodes: 1)
+# end
