40d0529efb5986219e26f7634f37922a823123b1
magnus-thor <magnus.thor.ar@gmail.com>
2018-06-04 17:21:39 +0200

    moved ES before block to helper file

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,20 +1,20 @@
-# Before('@search') do
-#   Chewy.strategy(:bypass)
-#   ArticleIndex.delete! if ArticleIndex.exists?
-#   if use_test_cluster? 
-#     Elasticsearch::Model.client = Elasticsearch::Client.new(host: "localhost:9250")
-#     Elasticsearch::Extensions::Test::Cluster.start(
-#       port: 9250,
-#       nodes: 1,
-#       timeout: 120
-#     ) 
-#   end
-#   ArticleIndex.create! unless ArticleIndex.exists?
-# end
+Before('@search') do
+  Chewy.strategy(:bypass)
+  ArticleIndex.delete! if ArticleIndex.exists?
+  if use_test_cluster? 
+    Elasticsearch::Model.client = Elasticsearch::Client.new(host: "localhost:9250")
+    Elasticsearch::Extensions::Test::Cluster.start(
+        port: 9250,
+        nodes: 1,
+        timeout: 120
+    ) 
+  end
+  ArticleIndex.create! unless ArticleIndex.exists?
+end
 
-# def use_test_cluster?
-#   !ENV["CI"] && !Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
-# end
+def use_test_cluster?
+  !ENV["CI"] && !Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
+end
 
 # if use_test_cluster? 
 #   Elasticsearch::Model.client = Elasticsearch::Client.new(host: "localhost:9250")
