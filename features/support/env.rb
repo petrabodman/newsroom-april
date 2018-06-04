c53160ad267eb945ebe992a4e1c9ac2b4aa5874f
Agnes Konradsdottir <agneskonradsdottir@gmail.com>
2018-06-04 20:17:59 +0200

    Clean out commented out code

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,21 +15,3 @@ end
 Cucumber::Rails::Database.javascript_strategy = :truncation
 
 World(FactoryBot::Syntax::Methods)
-
-# Before do
-#   Chewy.strategy(:bypass)
-#   ArticleIndex.delete! if ArticleIndex.exists?
-#   if use_test_cluster? 
-#     Elasticsearch::Model.client = Elasticsearch::Client.new(host: "localhost:9250")
-#     Elasticsearch::Extensions::Test::Cluster.start(
-#       port: 9250,
-#       nodes: 1,
-#       timeout: 120
-#     ) 
-#   end
-#   ArticleIndex.create! unless ArticleIndex.exists?
-# end
-
-# def use_test_cluster?
-#   !ENV["CI"] && !Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
-# end
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,7 +2,6 @@ Before('@search') do
   Chewy.strategy(:bypass)
   ArticleIndex.delete! if ArticleIndex.exists?
   if use_test_cluster? 
-    Elasticsearch::Model.client = Elasticsearch::Client.new(host: "localhost:9250")
     Elasticsearch::Extensions::Test::Cluster.start(
         port: 9250,
         nodes: 1,
@@ -15,8 +14,3 @@ end
 def use_test_cluster?
   !ENV["CI"] && !Elasticsearch::Extensions::Test::Cluster.running?(on: 9250)
 end
-
-# if use_test_cluster? 
-#   Elasticsearch::Model.client = Elasticsearch::Client.new(host: "localhost:9250")
-#   Elasticsearch::Extensions::Test::Cluster.start(port: 9250, nodes: 1)
-# end
