62250800e80f921e66e872c2c40f563b6f0d95fb
George_Lewis <georgejlewis@hotmail.com>
2018-06-05 15:40:53 +0200

    rspec tst pass

--- spec/requests/api/v1/api_spec.rb
+++ spec/requests/api/v1/api_spec.rb
@@ -9,15 +9,16 @@ RSpec.describe Api::V1::ApiController, type: :request do
       get '/api/v1/api'
 
       json_response = JSON.parse(response.body)
-      expect(response.status).to eq 200
-      expect(json_response['article']).to eq([{"id"=>article.id,
+      expected_response = [{"id"=>article.id,
         "created_at"=>article.created_at,
         "updated_at"=>article.updated_at,
         "category_id"=>category.id,
         "user_id"=>user.id,
         "published"=>true,
         "headline"=>"dave",
-        "content"=>"is mank"}])
+        "content"=>"is mank"}]
+      expect(response.status).to eq 200
+      expect(json_response['article']).to eq expected_response.as_json
     end
   end
 end
