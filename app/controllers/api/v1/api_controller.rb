613329d800a3f592b2887057a892496d5bc3cc88
George_Lewis <georgejlewis@hotmail.com>
2018-06-05 15:52:00 +0200

    ammended device route as it was wrong

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,12 +1,4 @@
 class Api::V1::ApiController < ApplicationController
-  # protect_from_forgery with: :null_session
-  #
-  # before_action :destroy_session
-  #
-  # def destroy_session
-  #   request.session_options[:skip] = true
-  # end
-
   def index
     article = Article.all
     render json: { article: article }
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :api, only: [:index], constraints:{ format: :json }
-  devise_for :users
+    end
   end
-end
+  devise_for :users
   resources :articles, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :comments, only: [:new, :create, :index]
   end
