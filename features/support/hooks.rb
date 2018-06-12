3d6f6f328cffad9b03b10a943805449ed6e1704e
magnus-thor <magnus.thor.ar@gmail.com>
2018-06-12 09:43:40 +0200

    Switches ES ports in development its 9250 in test its 9200

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,29 +1,45 @@
+# Before('@search') do
+#   Chewy.strategy(:bypass)
+#   if ENV['CHEWY'] == 'ci'
+#     options = {
+#       port: 9200,
+#       nodes: 1,
+#       timeout: 120,
+#       command: "nohup bash -c \"$SEMAPHORE_PROJECT_DIR/elasticsearch/bin/elasticsearch 2>&1\""
+#     }
+#   else
+#     options = {
+#       port: 9200,
+#       nodes: 1,
+#       timeout: 120
+#     }
+#   end
+#   Elasticsearch::Extensions::Test::Cluster.start(options) 
+   
+#   while !Elasticsearch::Extensions::Test::Cluster.running? do 
+#     sleep 1
+#   end
+#   ArticleIndex.delete! if ArticleIndex.exists?
+#   ArticleIndex.create! unless ArticleIndex.exists?
+# end
+
+# After('@search') do 
+#   port = ENV['CHEWY'] == 'ci' ? 9200 : 9250 
+#   Elasticsearch::Extensions::Test::Cluster.stop(port: port)
+# end
+
 Before('@search') do
   Chewy.strategy(:bypass)
-  if ENV['CHEWY'] == 'ci'
-    options = {
-      port: 9200,
-      nodes: 1,
-      timeout: 120,
-      command: "nohup bash -c \"$SEMAPHORE_PROJECT_DIR/elasticsearch/bin/elasticsearch 2>&1\""
-    }
-  else
-    options = {
+  Elasticsearch::Extensions::Test::Cluster.start(
       port: 9200,
       nodes: 1,
       timeout: 120
-    }
-  end
-  Elasticsearch::Extensions::Test::Cluster.start(options) 
-   
-  while !Elasticsearch::Extensions::Test::Cluster.running? do 
-    sleep 1
-  end
+  )
+
   ArticleIndex.delete! if ArticleIndex.exists?
   ArticleIndex.create! unless ArticleIndex.exists?
 end
 
 After('@search') do 
-  port = ENV['CHEWY'] == 'ci' ? 9200 : 9250 
-  Elasticsearch::Extensions::Test::Cluster.stop(port: port)
+  Elasticsearch::Extensions::Test::Cluster.stop(port: 9200)
 end
